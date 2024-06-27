b27817b17ab967d0497a7e3dddd6643942ed87a7
mgcllee <mgcllee@gmail.com>
2024-06-27 15:54:28 +0900

    [Server][Main, Stage] MainServer에서 StageServer 구동 테스트

--- RHTF_Server/RHTFMainServer/Program.cs
+++ RHTF_Server/RHTFMainServer/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
 
 namespace RHTFMainServer
 {
@@ -10,7 +14,31 @@ namespace RHTFMainServer
     {
         static void Main(string[] args)
         {
+            string exe_name = "C:\\Git\\RHTF\\RHTF_Server\\x64\\Release\\RHTFStageServer.exe";
+            
+            // 현재, 명령어의 띄어쓰기는 제거됨
+            Process.Start(exe_name, "C#에서 보내는 명령어 모음\nUID: Mgcllee, leemc\nnewAddr:127.0.0.1:7777");
 
+
+            // 모든 프로그램을 강.제.로. 종료시키는 반복문
+            // 따라서 특정 프로그램을 종료시켜야 할 필요 있음.
+            foreach (Process process in Process.GetProcesses())
+            {
+                // "exe_name"라는 이름을 가진 프로세스가 존재하면 true를 리턴한다.
+                if (process.ProcessName.StartsWith("RHTFStageServer"))
+                {
+                    // 프로세스를 죽이는 함수
+                    // Console.WriteLine("Kill!");
+                    // process.Kill();
+
+                    Console.WriteLine("Find!");
+                }
+            }
+
+            Console.WriteLine("C# 프로그램 종료");
+            Console.ReadLine();
+
+            // Continue;
         }
     }
 }
--- RHTF_Server/RHTFStageServer/Main.cpp
+++ RHTF_Server/RHTFStageServer/Main.cpp
@@ -2,9 +2,25 @@
 
 #include "stdafx.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+	printf("This is C++ base StageServer Program\n");
 	
+	// argv[0]은 실행 경로이다.
+	for (int i = 1; i < argc; ++i)
+	{
+		printf("%s", argv[i]);
+	}
+	printf("\n");
+
+
+	int n;
+	std::cin >> n;
+	// Continue;
+	while (false)
+	{
+
+	}
 
 	return 0;
 }
