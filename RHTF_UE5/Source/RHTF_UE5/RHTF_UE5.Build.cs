0cda7735b763e309d52d74ac1c6736be04876288
mgcllee <mgcllee@gmail.com>
2024-07-17 15:15:54 +0900

    [UE5][Build] update comment
    주석 추가.

--- RHTF_UE5/Source/RHTF_UE5/RHTF_UE5.Build.cs
+++ RHTF_UE5/Source/RHTF_UE5/RHTF_UE5.Build.cs
@@ -8,10 +8,10 @@ public class RHTF_UE5 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        // CppStandard = CppStandardVersion.Cpp17;
-
+        // Public 폴더에 있는 .h 파일에서 선언용.
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Sockets", "Networking" });
             
+        // Private 폴더에 있는 .cpp 파일에서만 사용한다.
 		PrivateDependencyModuleNames.AddRange(new string[] { "ProtobufCore" });
     }
 }
