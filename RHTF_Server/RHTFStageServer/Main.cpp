852958b395f4bf0d9422315b83597ddcd1666af4
mgcllee <mgcllee@gmail.com>
2024-07-22 13:55:48 +0900

    [Server][IOCP] ready for iocp module

--- RHTF_Server/RHTFStageServer/Main.cpp
+++ RHTF_Server/RHTFStageServer/Main.cpp
@@ -1,12 +1,30 @@
+/*
+[Homogeneous Thread queue]
+Coroutine || jthread..
+*/
+
 #pragma once
 
 #include "stdafx.h"
 #include <fstream>
 
+// Winsock2에서 업데이트된 헤더
 #include <WS2tcpip.h>
-#include <WinSock2.h>
+
+/*
+WSA는 Windows Socket API 의 약자로
+인터넷 네트워크 및 소켓 관련 함수를 제공 해주는 API
+Winsock은 version 1과 2가 있으며, 1 에서 기능이 추가된 것이 2 이다.
+
+위와 같은 맥락으로
+OVERLAPPED 구조체에 WSA 접두어를 붙인 것이
+WSAOVERLAPPED 구조체이다. 정의를 찾아보면 같다.
+*/
+#include <WS2tcpip.h>
 
 #pragma comment(lib, "ws2_32")
+
+HANDLE h_iocp;
 
 #include <vector>
 
@@ -14,10 +32,7 @@
 
 #define PORT_NUM 7777
 
-/*
-Coroutine || jthread..
-
-*/
+std::vector<SOCKET> client(2);  // 임시 객체(Unity 1 + UE5 1)
 
 int main(int argc, char* argv[])
 {
@@ -32,11 +47,20 @@ int main(int argc, char* argv[])
     server_addr.sin_port = htons(PORT_NUM);
     server_addr.sin_addr.S_un.S_addr = INADDR_ANY;
 
-    std::vector<SOCKET> client(2);
+    // h_iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 0);
+    // CreateIoCompletionPort(reinterpret_cast<HANDLE>(Listen_Sock), h_iocp, 0, 0);
+
     int ticket = 0;
 
     while (true)
     {
+        /*DWORD cnt_byte;
+        ULONG_PTR key;
+        WSAOVERLAPPED* over = nullptr;
+
+        GetQueuedCompletionStatus(h_iocp, &cnt_byte, &key, &over, INFINITE);
+        reinterpret_cast<*>(over);*/
+
         printf("[Server] wait for new client...\n");
 
         bind(Listen_Sock, (sockaddr*)(&server_addr), sizeof(server_addr));
